5d75b6a36de0fb65e0ad9977f3917d1b8cd7da51
dogTHEsquid <81450231+dogTHEsquid@users.noreply.github.com>
2021-03-27 14:36:15 -0400

    Maps!

---
+++ GoogleMaps/Maps.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Horo on 3/27/2021.
+ */
+$( function() {
+    accordion.init({
+        id: 'accordion'
+    });
+} );
+
+
+
--- GoogleMaps/Overlay.js
+++ GoogleMaps/Overlay.js
@@ -1,103 +1,3 @@
-/**
- * Created by Horo on 3/26/2021.
- */
-// This example creates a custom overlay called USGSOverlay, containing
-// a U.S. Geological Survey (USGS) image of the relevant area on the map.
-// Set the custom overlay object's prototype to a new instance
-// of OverlayView. In effect, this will subclass the overlay class therefore
-// it's simpler to load the API synchronously, using
-// google.maps.event.addDomListener().
-// Note that we set the prototype to an instance, rather than the
-// parent class itself, because we do not wish to modify the parent class.
-// Initialize the map and the custom overlay.
-
-/*
- function initMap() {
- const map = new google.maps.Map(document.getElementById("map"), {
- zoom: 11,
- center: { lat: -25.344, lng: 131.036 },
- mapTypeId: "satellite",
- });
- const bounds = new google.maps.LatLngBounds(
- new google.maps.LatLng(-25.344, 131.036),
- new google.maps.LatLng(-25.744, 131.536)
- );
- // The photograph is courtesy of the U.S. Geological Survey.
- debugger;
- const srcImage = "../Images/Map.jpg";
-
- // The custom USGSOverlay object contains the USGS image,
- // the bounds of the image, and a reference to the map.
- class MapOverlay extends google.maps.OverlayView {
- constructor(bounds, image) {
- super();
- // Initialize all properties.
- this.bounds_ = bounds;
- this.image_ = image;
- // Define a property to hold the image's div. We'll
- // actually create this div upon receipt of the onAdd()
- // method so we'll leave it null for now.
- this.div_ = null;
- }
- //
- //         * onAdd is called when the map's panes are ready and the overlay has been
- //      * added to the map.
-
- onAdd() {
- this.div_ = document.createElement("div");
- this.div_.style.borderStyle = "none";
- this.div_.style.borderWidth = "0px";
- this.div_.style.position = "absolute";
- // Create the img element and attach it to the div.
- const img = document.createElement("img");
- img.src = this.image_;
- img.style.width = "100%";
- img.style.height = "100%";
- img.style.position = "absolute";
- this.div_.appendChild(img);
- // Add the element to the "overlayLayer" pane.
- const panes = this.getPanes();
- panes.overlayLayer.appendChild(this.div_);
- }
- draw() {
- // We use the south-west and north-east
- // coordinates of the overlay to peg it to the correct position and size.
- // To do this, we need to retrieve the projection from the overlay.
- const overlayProjection = this.getProjection();
- // Retrieve the south-west and north-east coordinates of this overlay
- // in LatLngs and convert them to pixel coordinates.
- // We'll use these coordinates to resize the div.
- const sw = overlayProjection.fromLatLngToDivPixel(
- this.bounds_.getSouthWest()
- );
- const ne = overlayProjection.fromLatLngToDivPixel(
- this.bounds_.getNorthEast()
- );
-
- // Resize the image's div to fit the indicated dimensions.
- if (this.div_) {
- this.div_.style.left = sw.x + "px";
- this.div_.style.top = ne.y + "px";
- this.div_.style.width = ne.x - sw.x + "px";
- this.div_.style.height = sw.y - ne.y + "px";
- }
- }
-
- //* The onRemove() method will be called automatically from the API if
- //* we ever set the overlay's map property to 'null'.
-
- onRemove() {
- if (this.div_) {
- this.div_.parentNode.removeChild(this.div_);
- this.div_ = null;
- }
- }
- }
- const overlay = new MapOverlay(bounds, srcImage);
- overlay.setMap(map);
- }
- */
-
 // This example adds hide() and show() methods to a custom overlay's prototype.
 // These methods toggle the visibility of the container <div>.
 // overlay to or from the map.
@@ -218,15 +118,11 @@ function initMap() {
     const toggleDOMButton = document.createElement("button");
     toggleDOMButton.textContent = "Toggle DOM Attachment";
     toggleDOMButton.classList.add("custom-map-control-button");
-    toggleButton.addEventListener("click", () => {
-        overlay.toggle();
-});
-    toggleDOMButton.addEventListener("click", () => {
-        overlay.toggleDOM(map);
+    toggleButton.addEventListener("click", () => {overlay.toggle(); });
+    toggleDOMButton.addEventListener("click", () => {overlay.toggleDOM(map);
 });
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDOMButton);
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
-
 
     infoWindow = new google.maps.InfoWindow();
     const locationButton = document.createElement("button");
@@ -247,34 +143,21 @@ function initMap() {
         infoWindow.open(map);
         map.setCenter(pos);
     }
-
-    , () => {handleLocationError(true, infoWindow, map.getCenter());} );
+    , () => {handleLocationError(true, infoWindow, map.getCenter()); } );
     }
 else {
         infoWindow.setContent("Geolocation is not supported.");
     }
 });
 
-    function handleLocationSuccess(position, infoWindow){
-        const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-        };
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
-        infoWindow.open(map);
-        map.setCenter(pos);
-    }
-
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
         infoWindow.setContent(
             browserHasGeolocation
-                ? "Error: The Geolocation service failed. " + geolocationPositionErrorInstance.code
+                ? "Error: The Geolocation service failed. "
                 : "Error: Your browser doesn't support geolocation."
         );
         infoWindow.open(map);
     }
 
 }
-
