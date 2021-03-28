05e0f36bfc1a1af0d986eef0a6b2460c5fbdede3
Kin-Wai Lam <kinwai.lam730@gmail.com>
2021-03-27 22:09:09 -0400

    Merge commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,13 +6,8 @@ $(document).ready(() => {
     })
 
     $(".select").on("click", () => {
-<<<<<<< HEAD
-        var conceptName = $('.form-select').find(":selected").text();
-        console.log(conceptName)
-=======
         let dropDown = $(".form-select :selected").val();
         console.log(dropDown)
->>>>>>> 802132eaab1702db4defd8590ee756d443b28a11
     })
 
 })
