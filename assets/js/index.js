2b66b560bc0e09b9b77ea2703a483829555a26b3
Kin-Wai Lam <kinwai.lam730@gmail.com>
2021-03-27 21:00:38 -0400

    merging

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,8 @@ $(document).ready(() => {
     })
 
     $(".select").on("click", () => {
-        let location = $(".form-select")
-        console.log(location)
-
+        var conceptName = $('.form-select').find(":selected").text();
+        console.log(conceptName)
     })
 
 })
